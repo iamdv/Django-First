ad0d4301fc882b8b6a56c1836e1ba47aa4c2ad2d
DV <me@iamdv.io>
2020-04-13 00:12:40 +0100

    learnt json dumps and loads

--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,17 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+
+# sample json
+userJSON = '{"first_name": "John", "last_name": "Doe", "age": 39}'
+
+# Parse to Dict
+user = json.loads(userJSON)
+
+print(user)
+print(type(user['first_name']))
+
+car = {'make': 'ford', 'year': 1970}
+carJSON = json.dumps(car)
+
+print(carJSON)
+print(type(carJSON))
